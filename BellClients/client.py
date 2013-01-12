c03d024a3d73cec55d99da8f5948b772c5fb544d
Kevin Valk <kevin@omniasoft.nl>
2013-01-12 17:57:28 +0100

    Real crude BellPi client in python3 for testing purpuses

---
+++ BellClients/client.py
@@ -0,0 +1,31 @@
+import socket
+import sys
+import time
+
+HOST, PORT = "localhost", 8311
+clientKey = 0xCAADBCB7
+packet = [0xB7, 0xBC, 0xAD, 0xCA, 5, 2, 0, 0, 0]
+packet2 = [0xB7, 0xBC, 0xAD, 0xCA, 5, 3, 0, 0, 0]
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to server and send data
+sock.connect((HOST, PORT))
+
+# Try first to ask something w/o reg
+#sock.sendall(bytes(pInfo + "\n", "utf-8"))
+#received = str(sock.recv(1024), "utf-8")
+#print("Received: {}".format(received))
+
+# Now reg
+sock.sendall(bytearray(packet))
+time.sleep(1)
+sock.sendall(bytearray(packet2))
+time.sleep(1)
+received = str(sock.recv(1024), "utf-8")
+print("Received: {}".format(received))
+
+sock.close()
+
+
